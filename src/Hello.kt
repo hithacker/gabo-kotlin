28719c25f473399131275782273bcd80a4a99648
Hiren Thacker <hithacker@gmail.com>
2017-06-28 16:36:48 +0530

    Started writing tokenize_chunk function

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,10 +1,19 @@
 fun main(args: Array<String>) {
-    var template =
+    val template =
             """"<p> Hello {{name}}, your friends are: </p>
  <ul>
  {{#friends}}
     <li>{{first-name}} {{last-name}} </li>
  {{/friends}}
  </ul>"""
-    println(Pair(template, 5).second)
+    val (match, token) = tokenize_chunk("{{bar}} foo")
+    println(match)
+    println(token)
+}
+
+fun tokenize_chunk(template: String) : Pair<String, String> {
+    val matchResult = Regex("""\A\{\{\s*([\w-.]+)\s*}}""").find(template)
+    val value = matchResult?.value ?: ""
+    val token: String = matchResult?.groupValues?.get(1) ?: ""
+    return Pair(value, token)
 }
